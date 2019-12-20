7abcab70ef9288728bb2403815b643fc805f89ff
Vinay <58210167+vinay-nandipur@users.noreply.github.com>
2019-12-20 13:58:47 +0530

    updated msrc_cve_reporter.ps1 to upload reports to AWS s3 bucket

--- msrc_cve_reporter.ps1
+++ msrc_cve_reporter.ps1
@@ -4,13 +4,13 @@ $dt2 = (get-date -Uformat %Y-%b).ToString()
 $os_name = Get-CimInstance Win32_OperatingSystem | Select Caption  -ExpandProperty Caption
 $os_name = $os_name.Split("") | Where-Object {$_ -notmatch "microsoft|standard|datacenter|enterprise"}
 $os_name = & {Write-Host -NoNewline $os_name} 6>&1
+$ami_id = (Invoke-RestMethod -uri http://169.254.169.254/latest/dynamic/instance-identity/document | Select-Object -ExpandProperty imageId).trim()
+$instance_id = (Invoke-RestMethod -uri http://169.254.169.254/latest/dynamic/instance-identity/document | Select-Object -ExpandProperty instanceId).trim()
 
 # Install MSRCSecurityUpdates Module
 
-if(-not {Get-Module MsrcSecurityUpdates})
- {
-     Install-Module MSRCSecurityUpdates -Confirm:$False -Force
- }
+Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+Install-Module MSRCSecurityUpdates -Confirm:$False -Force
 
 # Import MSRCSecurityUpdates Module
 
@@ -18,7 +18,7 @@ Import-Module MSRCSecurityUpdates
 
 # Configure API Key
 
-Set-MSRCApiKey -ApiKey "<API_Key>" -Verbose
+Set-MSRCApiKey -ApiKey "<api-key>" -Verbose
 
 # Get CVE Reports as HTML report for current month
 
@@ -33,13 +33,18 @@ $CVRFDoc = Get-MsrcCvrfDocument -ID $dt2 -Verbose
 $cve_list = Get-MsrcCvrfAffectedSoftware -Vulnerability $CVRFDoc.Vulnerability -ProductTree $CVRFDoc.ProductTree | Where-Object {$_.FullProductName -match $os_name} | Select CVE -ExpandProperty CVE | Where-Object {$_ -notmatch "ADV"} | Get-Unique
 
 $CVRFHtmlProperties = @{
-   Vulnerability = $CVRFDoc.Vulnerability | Where-Object {$_.CVE -in $cve_list}
-   ProductTree = $CVRFDoc.ProductTree
-   DocumentTracking = $CVRFDoc.DocumentTracking
-   DocumentTitle = $CVRFDoc.DocumentTitle
+ Vulnerability = $CVRFDoc.Vulnerability | Where-Object {$_.CVE -in $cve_list}
+ ProductTree = $CVRFDoc.ProductTree
+ DocumentTracking = $CVRFDoc.DocumentTracking
+ DocumentTitle = $CVRFDoc.DocumentTitle
 }
 
-Get-MsrcSecurityBulletinHtml @CVRFHtmlProperties -Verbose | Out-File c:\temp\MSRCSecurityUpdates-local-$dt2.html
+Get-MsrcSecurityBulletinHtml @CVRFHtmlProperties -Verbose | Out-File c:\temp\MSRCSecurityUpdates-$ami_id-$instance_id-$dt2.html
+
+aws s3 cp c:\temp\MSRCSecurityUpdates-$dt2.html s3://<bucket-name>
+aws s3 cp c:\temp\MSRCVulnerabilities-$dt2.html s3://<bucket-name>
+aws s3 cp c:\temp\MSRCSecurityUpdates-$ami_id-$instance_id-$dt2.html s3://<bucket-name>
+
 
 # Verify status of Security patches
 
@@ -49,22 +54,24 @@ $installed_kb_list = Get-HotFix | select HotFixID -ExpandProperty HotFixID
 
 Foreach ($kb in $latest_kb_list)
 {
- if ($installed_kb_list -Contains $kb)
 
- {
+if(-not {$installed_kb_list -Contains $kb})
 
-    Write-Output "KB $KB patched as per $dt2 MSRC Security Updates" | out-file -Append c:\temp\security_patches_status.txt
+{
 
- }
+ Write-Output "KB $KB should be patched ASAP!" | out-file -Append c:\temp\security_patches_status.txt
 
+}
 
- else
+else
 
- {
+{
 
-   Write-Output "KB $KB should be patched ASAP!" | out-file -Append c:\temp\security_patches_status.txt
+ Write-Output "KB $KB patched as per $dt2 MSRC Security Updates" | out-file -Append C:\Temp\security_patches_status_all.txt
 
- }
+}
 
 
 }
+
+Get-Content C:\Temp\security_patches_status_all.txt | Select-Object -Unique | Out-File C:\Temp\security_patches_status.txt
